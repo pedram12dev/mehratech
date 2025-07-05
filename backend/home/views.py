f776dd4e45c272eb1e3bb1c4a6360615b974dc09
pedram <pedram.najafi73@gmail.com>
2025-07-05 23:25:20 +0330

    create home view

--- backend/home/views.py
+++ backend/home/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.views import APIView 
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Home
+from .serializers import HomePageSerializer
 
-# Create your views here.
+
+class HomePageView(APIView):
+    """
+    API view to handle Home page data.
+    """
+
+    def get(self, request):
+        """
+        Retrieve the home page data.
+        """
+        home = Home.objects.all()
+        if home:
+            serializer = HomePageSerializer(home,many=True, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({"detail": "Home page not found."}, status=status.HTTP_404_NOT_FOUND)
