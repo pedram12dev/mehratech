9347829ab5e1f037bedd7e0391af0429b5c341f5
pedram <pedram.najafi73@gmail.com>
2025-07-05 23:24:58 +0330

    create home models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Home(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Title")
+    description = models.TextField(verbose_name="Description")
+    logo = models.ImageField(upload_to='home_images/', verbose_name="Logo")
+
+    class Meta:
+        verbose_name = "Home"
+        verbose_name_plural = "Homes"
+
+    def __str__(self):
+        return self.title
