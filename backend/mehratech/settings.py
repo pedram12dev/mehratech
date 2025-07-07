1560c0ffb96c773dbb5471d6ef9b533e00d13271
pedram <pedram.najafi73@gmail.com>
2025-07-07 23:07:50 +0330

    add corsheaders setting

--- backend/mehratech/settings.py
+++ backend/mehratech/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
 
     # third-party apps
     'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,3 +138,8 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://37.32.13.184",
+    "http://localhost:3000",
+]
